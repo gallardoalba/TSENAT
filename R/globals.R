5de4b5810e59db70975ca4b828c41e65e4b68bac
CI Deployment <ci@example.com>
2026-02-10 21:52:27 +0100

    Prune maintenance branch: keep only tools/ (remove other files)

--- R/globals.R
+++
@@ -1,7 +0,0 @@
-# # Declare package global variables to satisfy R CMD check NOTES
-if (getRversion() >= "2.15.1") {
-    utils::globalVariables(c(
-        "genes", "xval", "x", "y", "padj", "padj_num", "padj_clean",
-        "label_flag", "sample_q", "qnum", "significant"
-    ))
-}
